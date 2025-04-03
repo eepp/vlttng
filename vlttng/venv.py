0ed0ccc838064151c8599f5d6ac07ed755729874
Philippe Proulx <eeppeliteloop@gmail.com>
2025-04-03 13:09:07 -0400

    vlttng/venv.py: update dead Apache log4j download URL
    Signed-off-by: Philippe Proulx <eeppeliteloop@gmail.com>

--- vlttng/venv.py
+++ vlttng/venv.py
@@ -726,7 +726,7 @@ class VEnvCreator:
             log4j1_jar = '{}.jar'.format(log4j1_name)
             _pinfo('Download Apache log4j')
             self._runner.cd(self._paths.src)
-            self._runner.wget('http://apache.mirror.gtcomm.net/logging/log4j/1.2.17/{}'.format(log4j1_tarball),
+            self._runner.wget('https://archive.apache.org/dist/logging/log4j/1.2.17/{}'.format(log4j1_tarball),
                               log4j1_tarball)
 
             # extract
